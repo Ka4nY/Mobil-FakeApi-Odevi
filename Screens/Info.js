45db1400882e3c2d0cb15e8c62091544522624ea
Ka4nY <kaanyuceyurt2008@gmail.com>
2024-02-27 23:48:41 +0300

    first commit

---
+++ Screens/Info.js
@@ -0,0 +1,29 @@
+import {SafeAreaView,Text} from 'react-native';
+
+export default function Info ({navigation}) {
+  return (
+    <SafeAreaView>
+     <Text>Ürün1:
+     Bu ürünümüz Amerikadan getirtilmekte ve kaliteli bir maldır.Ürün iadesi 2 haftadan sonra geçersizdir.
+     </Text>
+     <Text>Ürün2:
+     Bu ürünümüz İngiltereden getirtilmekte ve kaliteli bir maldır.Ürün iadesi 3 haftadan sonra geçersizdir.</Text>
+     <Text>Ürün3:
+     Bu ürünümüz Hindistandan getirtilmekte ve kaliteli bir maldır.Ürün iadesi 3 haftadan sonra geçersizdir.</Text>
+     <Text>Ürün4:
+     Bu ürünümüz Amerikadan getirtilmekte ve kaliteli bir maldır.Ürünün 1 aylık garantisi vardır.</Text>
+     <Text>Ürün5:
+     Bu ürünümüz İngiltereden getirtilmekte ve kaliteli bir maldır.Ürünün 3 aylık garantisi vardır.</Text>
+     <Text>Ürün6:
+     Bu ürünümüz İngiltereden getirtilmekte ve kaliteli bir maldır.Ürünün 3 aylık garantisi vardır.</Text>
+     <Text>Ürün7:
+     Bu ürünümüz Kanadadan getirtilmekte ve kaliteli bir maldır.Ürünün garantisi yoktur.</Text>
+     <Text>Ürün8:
+     Bu ürünümüz Amerikadan getirtilmekte ve kaliteli bir maldır.Ürünün 2 aylık garantisi vardır.</Text>
+     <Text>Ürün9:
+     Bu ürünümüz İngiltereden getirtilmekte ve kaliteli bir maldır.Ürünün 1 yıllık garantisi vardır.</Text>
+     <Text>Ürün10:
+     Bu ürünümüz İngiltereden getirtilmekte ve kaliteli bir maldır.Ürünün 1,5 aylık garantisi vardır.</Text>
+    </SafeAreaView>
+  );
+}
